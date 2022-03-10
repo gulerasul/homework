e07650cb78590c08d2b6ff97000b1ce8601ad1b4
Guler Resulova <tu699evkp@aztu.local>
2022-03-10 14:14:50 +0400

    new

---
+++ task/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 8, 13, 15, 26, 38, 49, 52 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("");
+
+            Change(numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+        static void Change (int []numbers)
+        {
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (min > numbers[i])   
+                {
+                    min =i;
+                }
+                if (max<numbers[i])
+                {
+                    max = i;
+                }
+            }
+            numbers[max] = numbers[min] + numbers[max];
+            numbers[min] = numbers[max] - numbers[min];
+            numbers[max] = numbers[max] - numbers[min];
+
+        }
+
+    }
+}
